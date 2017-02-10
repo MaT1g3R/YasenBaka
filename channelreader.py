db5a1c348254dbd5fc0d914075472e59ea8e7a75
MaT1g3R <982341777@qq.com>
2017-02-10 17:22:32 -0500

    Cleverbot API is dead, removed chat bot function atm

--- channelreader.py
+++ channelreader.py
@@ -1,5 +1,4 @@
 """Handles mointoring of text channels"""
-from cleverbot import Cleverbot
 from random import choice
 
 
@@ -12,7 +11,6 @@ class ChannelReader:
         self.bot_nick = '<@!243230010532560896>'
         self.bot_name = 'Yasen-Baka#6539'
         self.bot = bot
-        self.clever = {}
         with open('lewd.txt') as f:
             self.lewd = f.read().splitlines()
         self.lewd.append('( ͡° ͜ʖ ͡°)')
@@ -25,20 +23,11 @@ class ChannelReader:
         :rtype: None
         """
         # Local vars
-        author_name = message.author.name
+        # author_name = message.author.name
         lowerstr = str(message.content).lower()
         # Clever bot functions
-        if message.content.startswith(self.bot_id or self.bot_nick):
-            if message.content.startswith(self.bot_id):
-                temp_bot_id = self.bot_id
-            else:
-                temp_bot_id = self.bot_nick
-            if author_name not in self.clever:
-                self.clever[author_name] = Cleverbot(author_name)
-            await self.bot.send_message(message.channel, "To <@{}>: ".format(message.author.id) + self.clever
-                                        [author_name].ask(message.content.replace(temp_bot_id, '')))
         # Sends ayaya pic when someone says ayaya
-        elif "ayaya" in lowerstr and not lowerstr.startswith("enqueued ") and not lowerstr.startswith("now playing "):
+        if "ayaya" in lowerstr and not lowerstr.startswith("enqueued ") and not lowerstr.startswith("now playing "):
             await self.bot.send_message(message.channel, "http://i.imgur.com/g3Qi8Ft.png")
         # Respnods to /o/ and \o\
         elif message.content == "/o/" and str(message.author) != self.bot_name:
