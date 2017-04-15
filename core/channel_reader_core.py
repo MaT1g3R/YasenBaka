382281d920d0606e19cf9d4559f9167b081fbc55
Umi <982341777@qq.com>
2017-04-14 21:10:47 -0400

    formatting

--- core/channel_reader_core.py
+++ core/channel_reader_core.py
@@ -73,12 +73,12 @@ def linux_meme(text: str):
 
 def interject():
     a = "\"I'd just like to interject for a moment. What you're referring " \
-           "to as Linux, is in fact, GNU/Linux, or as I've recently taken to " \
-           "calling it, GNU plus Linux. Linux is not an operating system " \
-           "unto itself, but rather another free component of a fully " \
-           "functioning GNU system made useful by the GNU corelibs, " \
-           "shell utilities and vital system components comprising a " \
-           "full OS as defined by POSIX."
+        "to as Linux, is in fact, GNU/Linux, or as I've recently taken to " \
+        "calling it, GNU plus Linux. Linux is not an operating system " \
+        "unto itself, but rather another free component of a fully " \
+        "functioning GNU system made useful by the GNU corelibs, " \
+        "shell utilities and vital system components comprising a " \
+        "full OS as defined by POSIX."
 
     b = "Many computer users run a modified version of the GNU system every " \
         "day, without realizing it. Through a peculiar turn of events, " \
