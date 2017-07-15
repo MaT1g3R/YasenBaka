e9f17c9b98e5d4cbd28b8ea5a4448702cc8a8e03
Umi <982341777@qq.com>
2017-07-15 05:21:29 -0400

    Added a script to clear cache

---
+++ clear_cache.py
@@ -0,0 +1,18 @@
+from itertools import chain
+
+from data import data_path
+
+
+def clean():
+    logs = data_path.joinpath('logs')
+    dumps = data_path.joinpath('dumps')
+    for file in chain(logs.iterdir(), dumps.iterdir()):
+        if not file.name.startswith('.'):
+            try:
+                file.unlink()
+            except FileNotFoundError:
+                continue
+
+
+if __name__ == '__main__':
+    clean()
