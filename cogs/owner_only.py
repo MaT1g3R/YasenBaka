d24fb97341a2c87f743a98a5c771f81b6436f563
MaT1g3R <982341777@qq.com>
2017-11-16 21:30:16 -0500

    Add an announcement command

--- cogs/owner_only.py
+++ cogs/owner_only.py
@@ -1,4 +1,6 @@
-from discord.ext.commands import Context
+from discord import Forbidden, TextChannel
+from discord.embeds import Embed
+from discord.ext.commands import Context, command
 
 from bot import Yasen
 from scripts.checks import is_owner
@@ -12,3 +14,48 @@ class OnwerOnly:
 
     async def __local_check(self, ctx: Context):
         return await is_owner(ctx)
+
+    @command()
+    async def announce(self, ctx: Context, *msg):
+        """
+        Send an announcement to every guild.
+
+        This is hidden in the help message
+        """
+        res = Embed(
+            colour=self.bot.config.colour,
+            title='This is an announcement from my developer.',
+            description=msg
+        )
+        await send_anncoucements(self.bot, res)
+
+
+async def send_anncoucements(bot: Yasen, embed: Embed):
+    for guild in bot.guilds:
+        for ch in sorted(guild.channels, key=announce_key):
+            if not isinstance(ch, TextChannel):
+                continue
+            try:
+                await ch.send(embed=embed)
+                break
+            except Forbidden:
+                continue
+
+
+def announce_key(channel):
+    if not isinstance(channel, TextChannel):
+        return 5
+
+    if channel.is_nsfw():
+        return 4
+    name = channel.name.lower()
+    if 'announce' in name:
+        return 0
+
+    if 'general' in name:
+        return 1
+
+    if 'bot' in name:
+        return 2
+
+    return 3
